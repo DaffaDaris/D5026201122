fda5b99c9265d582e8c8f967f98998a8dc79bc1c
Daffa Daris Mahendra Ansori <5026201122@mahasiswa.integra.its.ac.id>
2021-11-24 09:39:28 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,12 @@ Route::post('sayhi',"ViewController@sayHi") ;
 Route::get('PHP',"ViewController@showtugasPHP") ;
 Route::post('isiann',"ViewController@isian") ;
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
 
