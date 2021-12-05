608e100513fd235125a986df9f7e5e34886a7177
Daffa Daris Mahendra Ansori <5026201122@mahasiswa.integra.its.ac.id>
2021-12-05 15:40:22 +0700

    Absen

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,13 @@ Route::get('/tugas/edit/{id}','TugasController@edit');
 Route::post('/tugas/update','TugasController@update');
 Route::get('/tugas/hapus/{id}','TugasController@hapus');
 
+//route tabel absen
+Route::get('/absen','AbsenController@index');
+Route::get('/absen/tambah','AbsenController@tambah');
+Route::post('/absen/store','AbsenController@store');
+Route::get('/absen/edit/{id}','AbsenController@edit');
+Route::post('/absen/update','AbsenController@update');
+Route::get('/absen/hapus/{id}','AbsenController@hapus');
+
+
 
